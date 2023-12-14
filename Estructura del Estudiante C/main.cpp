348324e8cbf274506b29c844a31794520d516cfc
ddealsan <ddealsan@myuax.com>
2023-12-14 11:59:51 +0100

    creamos la funcion mostrarEstudiante

--- Estructura del Estudiante C/main.cpp
+++ Estructura del Estudiante C/main.cpp
@@ -9,4 +9,8 @@ struct Estudiante{
     float promedio
 };
 
-void mostrarEstudiante
+void mostrarEstudiante(Estudiante estudiante){
+    cout << "Nombre: " << estudiante.nombre << endl;
+    cout << "Edad: " << estudiante.edad << endl;
+    cout << "Promedio: " << estudiante.promedio << endl;
+}

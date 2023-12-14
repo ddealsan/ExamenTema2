89598772cb7053771a7e3a95a92f25038e02ae6d
ddealsan <ddealsan@myuax.com>
2023-12-14 12:17:02 +0100

    Copiamos y pegamos el anterior codigo

--- GestionEstudianteCPP/main.cpp
+++ GestionEstudianteCPP/main.cpp
@@ -1,3 +1,25 @@
-//
-// Created by dalfo on 14/12/2023.
-//
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+struct Estudiante{
+    string nombre;
+    int edad;
+    float promedio;
+};
+
+void mostrarEstudiante(Estudiante estudiante){
+    cout << "Nombre: " << estudiante.nombre << endl;
+    cout << "Edad: " << estudiante.edad << endl;
+    cout << "Promedio: " << estudiante.promedio << endl;
+}
+
+int main(){
+    Estudiante estudiante1;
+    estudiante1.nombre = "Juan";
+    estudiante1.edad = 20;
+    estudiante1.promedio = 9.5;
+    mostrarEstudiante(estudiante1);
+    return 0;
+}

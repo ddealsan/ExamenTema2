70647eef8e9dba6380a15628f3e376b93149ee53
ddealsan <ddealsan@myuax.com>
2023-12-14 12:57:57 +0100

    Avanzamos codigo

--- GstionDelEstudianteC/main.c
+++ GstionDelEstudianteC/main.c
@@ -4,3 +4,11 @@
 
 #define MAX_MATERIAS 10
 #define MAX_NOMBRE 50
+
+struct Estudiante {
+    char nombre[MAX_NOMBRE];
+    int edad;
+    float promedio;
+    char materias[MAX_MATERIAS][MAX_NOMBRE];
+    int numMaterias;
+};

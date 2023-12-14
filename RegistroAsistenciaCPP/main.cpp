b876fc7bcace99b5e378c56d162b7bc8dc394555
ddealsan <ddealsan@myuax.com>
2023-12-14 12:35:13 +0100

    Avanzamos codigo

--- RegistroAsistenciaCPP/main.cpp
+++ RegistroAsistenciaCPP/main.cpp
@@ -22,11 +22,21 @@ void mostrarEstudiante(Estudiante estudiante){
     cout << "Promedio: " << estudiante.promedio << endl;
     cout << "Asistencias: " << endl;
     for (const auto& asistencia : estudiante.asistencias) {
-        cout << "Fecha: " << asistencia.fecha << " | Materia: " << asistencia.materia << " | Estado: " << asistencia.estado << endl;
+        cout << "Fecha: " << asistencia.nombre << " | Materia: " << asistencia.materia << " | Estado: " << asistencia.estado << endl;
     }
 }
 
 void registrarAsistencia(Estudiante& estudiante, const string& fecha, const string& materia, const string& estado) {
     Asistencia nuevaAsistencia = {fecha, materia, estado};
     estudiante.asistencias.push_back(nuevaAsistencia);
+}
+
+int main(){
+    Estudiante estudiante1;
+    estudiante1.nombre = "Juan";
+    estudiante1.edad = 20;
+    estudiante1.promedio = 9.5;
+    registrarAsistencia(estudiante1, "2023-01-02", "Historia", "falta");
+    registrarAsistencia(estudiante1, "2023-01-03", "Programación", "tardanza");
+    mostrarEstudiante(estudiante1);
 }

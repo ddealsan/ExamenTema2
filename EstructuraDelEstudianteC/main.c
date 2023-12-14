d90f04b397861bdf40f31599194cd348bcfd1dd8
ddealsan <ddealsan@myuax.com>
2023-12-14 12:52:40 +0100

    Avanzamos codigo

--- EstructuraDelEstudianteC/main.c
+++ EstructuraDelEstudianteC/main.c
@@ -12,3 +12,14 @@ void mostrarEstudiante(struct Estudiante estudiante) {
     printf("Edad: %d\n", estudiante.edad);
     printf("Promedio: %f\n", estudiante.promedio);
 }
+
+int main() {
+    struct Estudiante estudiante1;
+    strcpy(estudiante1.nombre, "Juan");
+    estudiante1.edad = 20;
+    estudiante1.promedio = 9.5;
+
+    mostrarEstudiante(estudiante1);
+
+    return 0;
+}

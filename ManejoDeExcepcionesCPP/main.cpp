a22e79f4249b360644405c86263f1c2bfcba01bb
ddealsan <ddealsan@myuax.com>
2023-12-14 12:39:57 +0100

    Avanzamos codigo

--- ManejoDeExcepcionesCPP/main.cpp
+++ ManejoDeExcepcionesCPP/main.cpp
@@ -14,3 +14,16 @@ class EstudianteException : public runtime_error {
 public:
     EstudianteException(const string& mensaje) : runtime_error(mensaje) {}
 };
+
+struct Estudiante {
+    string nombre;
+    int edad;
+    float promedio;
+    vector<Asistencia> asistencias;
+};
+
+struct Asistencia {
+    string fecha;
+    string materia;
+    string estado;
+};
